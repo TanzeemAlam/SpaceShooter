5b39396d3b0556f9d603c54c169efb5ef03d7861
TanzeemAlam <Tanzeemalam789@gmail.com>
2019-02-25 14:51:21 +0530

    Added manually
    Updated the image of the ship and also changed the canvas reference in sprite.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 //Creating a canvas
-
 var canvas = document.getElementById("myCanvas");
+
 
 var ctx = canvas.getContext("2d");
 
 canvas.width = 500;
 
 canvas.height = 500;
-
 
 //Creating an interval
 var FPS = 30;
@@ -36,12 +35,17 @@ var player ={
 	width: 32,
 	height: 32,
 	speed: 1,
+	//sprite: Sprite("player"),
+	
 	
 	draw_player: function(){
 	
 		ctx.fillSytle = this.color;
 	
 		ctx.fillRect(this.x,this.y,this.width,this.height);
+		
+		//this.sprite.draw(ctx, this.x, this.y);
+		
 	},
 
 	update_player: function() {
@@ -51,6 +55,7 @@ var player ={
 		$(document).keydown(function(e) {
     			switch(e.which) {
 				case 37: //left
+					//if(player.x > 0)
 					player.x -= player.speed;
 					break;
 
@@ -59,6 +64,7 @@ var player ={
 				//	break;
 		
 				case 39: // right
+					//if(player.x < canvas.width - player.width)
 					player.x += player.speed;
 					break;
 
@@ -86,6 +92,41 @@ var player ={
 	
 	
 };
+
+//midpoint function
+player.midpoint = function() {
+
+	return {
+		x: this.x + this.width/2,
+		y: this.y + this.height/2
+	};
+};
+
+//Shoot function
+player.shoot = function() {
+		
+		var bulletPosition = this.midpoint();
+		
+		playerBullets.push(Bullet({
+			speed: 5,
+			x: bulletPosition.x,
+			y: bulletPosition.y
+		}));		
+
+		//To set a random alert
+		//window.alert("Pew pew");
+		//To set the timeout interval
+		//window.setTimeout(shoot,1000);
+		
+};
+
+//Loading and drawing images
+//player.sprite = Sprite("player");
+
+//player.draw = function() {
+//	this.sprite.draw(ctx, this.x, this.y);
+//};
+
 
 //Creating Bullets
 function Bullet(I){
@@ -116,35 +157,65 @@ function Bullet(I){
 	};	
 
 	return I;
-}	
-
-//Shoot function
-
-player.shoot = function() {
-		
-		var bulletPosition = this.midpoint();
-		
-		playerBullets.push(Bullet({
-			speed: 5,
-			x: bulletPosition.x,
-			y: bulletPosition.y
-		}));		
-
-		//To set a random alert
-		//window.alert("Pew pew");
-		//To set the timeout interval
-		//window.setTimeout(shoot,1000);
-		
 };
 
-//midpoint function
-player.midpoint = function() {
 
-	return {
-		x: this.x + this.width/2,
-		y: this.y + this.height/2
+//Creating enemies
+var enemies = [];
+
+function Enemy(I) {
+	I = I || {};
+	I.active = true;
+	I.age = Math.floor(Math.random() * 128);
+	I.color = "#A2B";
+	I.x = canvas.width/4 + Math.random() * canvas.width/2;
+	I.y = 0;
+	I.xVelocity = 0;
+	I.yVelocity = 2;
+	I.width = 32;
+	I.heigth = 32;
+
+	I.sprite = Sprite("enemy");
+	
+	I.inBounds = function() {
+
+		return I.x >= 0 && I.x <= canvas.width && I.y >= 0 && I.y <= canvas.height;
 	};
+
+	I.draw = function() {
+
+		ctx.fillStyle = this.color;
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+
+		this.sprite.draw(ctx, this.x, this.y);
+	};
+
+	I.update = function() {
+
+		I.x += I.xVelocity;
+		I.y += I.yVelocity;
+		
+		I.xVelocity = 3 * Math.sin(I.age * Math.PI / 64);
+
+		I.age++;
+
+		I.active = I.active && I.inBounds();
+	};
+
+	return I;
 };
+
+//For collision detection
+//Player->Bullets
+//Player->Enemy
+function collides(a,b){
+	
+	return a.x < b.x + b.width && 
+		a.x + a.width > b.x &&
+		a.y < b.y + b.height &&
+		a.y + a.height > b.y;
+}
+
 
 //Function to update the axis/positon
 function update(){
@@ -156,13 +227,30 @@ function update(){
 	//Calling the update function in player
 	player.update_player();
 
+	//updating the bullets
 	playerBullets.forEach(function(bullet) {
 		bullet.update();
 	});
 
+	//filtering the bullets that are not collided and within canvas
 	playerBullets = playerBullets.filter(function(bullet) {
 		return bullet.active;
 	});
+
+	//updating the enemies
+	enemies.forEach(function(enemy) {
+		enemy.update();
+	});
+
+	//filtering the enemies that are not collided and within canvas
+	enemies = enemies.filter(function(enemy) {
+		return enemy.active;
+	});
+
+	//Pushing new enemies if almost all enemies are dead
+	if(Math.random() < 0.1) {
+		enemies.push(Enemy());
+	}
 
 }
 
@@ -173,16 +261,20 @@ function draw(){
 
 	ctx.fillStyle = "#000000"; //Set color to black
 
-	ctx.fillText("Sup Bro!", textX,textY);
+	//ctx.fillText("Sup Bro!", textX,textY);
 	
 	//Calling the draw function in player
 	player.draw_player();
 
+	//Drawing the bullets on shoot function
 	playerBullets.forEach(function(bullet) {
 		bullet.draw();
 	});
-
 	
+	//Drawing the enemies
+	enemies.forEach(function(enemy) {
+		enemy.draw();
+	});	
 
 }
 
